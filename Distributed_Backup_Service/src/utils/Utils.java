fda44f6bc16145877f725e3d23a94f994bfd51ec
Mário Fernandes <mgustavofernandes@gmail.com>
2018-02-27 16:10:26 +0000

    arguments validation started

---
+++ Distributed_Backup_Service/src/utils/Utils.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class Utils {
+	public static String defaultIP = "localhost";
+	public static enum operations {BACKUP, RESTORE, DELETE, RECLAIM, BACKUPENH, RESTOREENH, DELETEENH, RECLAIMENH, STATE};
+}

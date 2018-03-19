521779c74ce70a97c0ab295b9337e481b76b395b
Nelson Almeida <up201505394@fe.up.pt>
2018-03-19 16:03:23 +0000

    Add files via upload

--- Distributed_Backup_Service/src/utils/Message.java
+++ Distributed_Backup_Service/src/utils/Message.java
@@ -9,31 +9,61 @@ public class Message {
 	private int replicationDeg;
 	private String body;
 	
-	public Message() {};
+	public Message() {}
+
+	public String getOperation() {
+		return operation;
+	}
 	
-	public getFormat(String operation) {
-		String operation = "(PUTCHUNK)|(STORED)|(GETCHUNK)|(CHUNK)|(DELETE)|(REMOVED)";
-		String version = " [0-9]\\.[0-9]";
-		String senderId = " (0)|([0-9]+)";
-		String fileId = " [.]{64}";
-		String chunckNo = " [0-9]{1,6}";
-		String replicationDeg = " [1-9] ";
-		String crlf = "\r\n";
-		
-		switch(operation) {
-		case "PUTCHUNK":
-			//PUTCHUNK <Version> <SenderId> <FileId> <ChunkNo> <ReplicationDeg> <CRLF><CRLF><Body>
-			break;
-		case "STORED":
-		case "GETCHUNK":
-		case "CHUNK":
-		case "REMOVED":
-			//operation <Version> <SenderId> <FileId> <ChunkNo> <CRLF><CRLF>
-			break;
-		case "DELETE":
-			//DELETE <Version> <SenderId> <FileId> <CRLF><CRLF>
-			break;
-		}
-		
+	public String getVersion() {
+		return version;
+	}
+	
+	public int getSenderId() {
+		return senderId;
+	}
+	
+	public String getFileId() {
+		return fileId;
+	}
+	
+	public String getChunckNo() {
+		return chunckNo;
+	}
+	
+	public int getReplicationDeg() {
+		return replicationDeg;
+	}
+	
+	public String getBody() {
+		return body;
+	}
+	
+	public void setOperation(String operation) {
+		this.operation = operation;
+	}
+	
+	public void setVersion(String version) {
+		this.version = version;
+	}
+	
+	public void setSenderId(int senderId) {
+		this.senderId = senderId;
+	}
+	
+	public void setFileId(String fileId) {
+		this.fileId = fileId;
+	}
+	
+	public void setChunckNo(String chunckNo) {
+		this.chunckNo = chunckNo;
+	}
+	
+	public void setReplicationDeg(int replicationDeg) {
+		this.replicationDeg = replicationDeg;
+	}
+	
+	public void setBody(String body) {
+		this.body = body;
 	}
 }
--- Distributed_Backup_Service/src/utils/MessageInterpreter.java
+++ Distributed_Backup_Service/src/utils/MessageInterpreter.java
@@ -1,48 +1,85 @@
 package utils;
 
+import utils.Message;
+
 public class MessageInterpreter {
 
-	private String message;
+	private Message message = null;
+	private text;
 	
-	public MessageInterpreter(String message) {
-		this.message = message;
+	public MessageInterpreter(String text) {
+		this.text = text;
 	}
 	
-	public void run() {	
-		//Verify message header
-		String operation = "(PUTCHUNK)|(STORED)|(GETCHUNK)|(CHUNK)|(DELETE)|(REMOVED)";
+	public getOperationFormat(){
+		return "^(PUTCHUNK)|(STORED)|(GETCHUNK)|(CHUNK)|(DELETE)|(REMOVED) ";
+	}
+	
+	public getHeaderFormat(String operation) {
 		String version = " [0-9]\\.[0-9]";
 		String senderId = " (0)|([0-9]+)";
 		String fileId = " [.]{64}";
 		String chunckNo = " [0-9]{1,6}";
-		String replicationDeg = " [1-9] ";
+		String replicationDeg = " [1-9]";
 		String crlf = "\r\n";
-		String headerFormat = "^" + operation + version + senderId + fileId + chunckNo + replicationDeg + crlf + crlf;
-		if(!message.matches(headerFormat)) {
-			//incorrect message format
+		String format = "";
+		String common = operation + version + senderId + fileId;
+		
+		switch(operation) {
+		case "PUTCHUNK":
+			//PUTCHUNK <Version> <SenderId> <FileId> <ChunkNo> <ReplicationDeg> <CRLF><CRLF><Body>
+			format = common + chunckNo + replicationDeg + " " + crlf + crlf;
+			break;
+		case "STORED":
+		case "GETCHUNK":
+		case "CHUNK":
+		case "REMOVED":
+			//operation <Version> <SenderId> <FileId> <ChunkNo> <CRLF><CRLF>
+			format = common + chunckNo + " " + crlf + crlf+ "$";
+			break;
+		case "DELETE":
+			//DELETE <Version> <SenderId> <FileId> <CRLF><CRLF>
+			format = common + " " + crlf + crlf+ "$";
+			break;
 		}
+	}
+	
+	public void run() {	
+		//Verify operation
+		if(!text.matches(getOperationFormat())) {
+			return;
+		}	
+		//Extract operation
+		String operation = text.substring(0, text.indexOf(" "));
+		String rest = text.substring(text.indexOf(" ") + 1);
+		//Verify header
+		if(!text.matches(message.getHeaderFormat(operation))) {
+			return;
+		}
+		
+		message = new Message();
 		
 		//Extract message parameters
 		//Operation
-		operation = message.substring(0, message.indexOf(" "));
-		String rest = message.substring(message.indexOf(" ") + 1);
+		message.setOperation(operation);
 		//Version
-		version = rest.substring(0, message.indexOf(" "));
-		rest = rest.substring(message.indexOf(" ") + 1);
+		message.setVersion(rest.substring(0, rest.indexOf(" ")));
+		rest = rest.substring(rest.indexOf(" ") + 1);
 		//Sender id
-		senderId = rest.substring(0, message.indexOf(" "));
-		rest = rest.substring(message.indexOf(" ") + 1);
+		message.setSenderId(Integer.parseInt(rest.substring(0, rest.indexOf(" "))));
+		rest = rest.substring(rest.indexOf(" ") + 1);
 		//File id
-		fileId = rest.substring(0, message.indexOf(" "));
-		rest = rest.substring(message.indexOf(" ") + 1);
-		//Chunck number
-		chunckNo = rest.substring(0, message.indexOf(" "));
-		rest = rest.substring(message.indexOf(" ") + 1);
-		//Replication degree
-		replicationDeg = rest.substring(0, message.indexOf(" "));
-		rest = rest.substring(message.indexOf(" ") + 1);
-		
-		//Extract message body
-		
+		message.setFileId(rest.substring(0, rest.indexOf(" ")));
+		rest = rest.substring(rest.indexOf(" ") + 1);
+		//ChunkNo
+		if(operation != "DELETE")
+			message.setChunckNo(rest.substring(0, rest.indexOf(" ")));
+			rest = rest.substring(rest.indexOf(" ") + 1);
+		}
+		//Replication Degree and body
+		if(operation == "PUTCHUNK")
+			message.setReplicationDeg(Integer.parseInt(rest.substring(0, rest.indexOf(" "))));
+			message.setBody(rest.substring(rest.indexOf(" ") + 5));//+5 to ignore both crlf before body
+		}		
 	}
 }

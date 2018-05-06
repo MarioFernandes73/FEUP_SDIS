815e0397b73231afbc4eae3ec5a3c80c4659e248
André Baptista <aca.baptista@gmail.com>
2018-05-06 02:40:36 +0100

    Update Peer.java

--- Second_Project/src/peer/Peer.java
+++ Second_Project/src/peer/Peer.java
@@ -2,112 +2,149 @@ package peer;
 
 import messages.PacketHandler;
 import utils.Constants;
-import rmi.RMIInterface;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.SocketException;
 import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Peer implements RMIInterface{
+public class Peer {
 
-    private Integer id;
+    private static Integer id;
     private static boolean isBootPeer;
     private static String ip;
+    private static int port;
     private static String bootPeerIP;
-    private HashMap<Integer,Address> forwardingTable = new HashMap<>();
-    private static int networkSize;
+    private static int bootPeerPort;
+    private static HashMap<Integer,Address> forwardingTable = new HashMap<>();
     private static int peerLimit;
-    private DatagramSocket socket;
+    private static int networkSize;
+    private static DatagramSocket socket;
     
+    public static void main(String[] args) throws IOException {
 
-    public Peer(Integer id, boolean isBootPeer) throws SocketException {
-        this.id = id;
-        this.isBootPeer = isBootPeer;
-        socket = new DatagramSocket();
-    }
-    
-    public static void main(String[] args) {
-
-    	if(setupPeer(args) != 0)
+    	if(!verifyArgs(args))
     		return;
     	
-    	/*...*/
-    	
-    }
-    
-    public static int setupPeer(String args[]){
+        ip = getPublicIP();
 
-    	int verify = verifyArgs(args);
-    	
-    	switch(verify)
-    	{
-    		case 0:
-    			System.out.println("Setup successuful.");
-    			break;
-    		case 1:
-    			System.out.println("Incorrect number of arguments.");
-    			break;
-    		case 2: 
-    			System.out.println("Error. First argument should be 'boot' or 'normal'");
-    			break;
-    		case 3:
-    			System.out.println("Error. Invalid peer limit. Should be 1-1000");
-    			break;
-    		case 4:
-    			System.out.println("Error. Invalid IP:PORT format");
-    			break;
-    	}
-		
-    	return verify;
-    	
+        if(isBootPeer)
+        	receiveNewPeer();
+        else
+        	connectToBootPeer();
+   
     }
-    
-    public static int verifyArgs(String args[])
+
+    public static boolean verifyArgs(String args[])
     {
-    	if(args.length != 2)
-    		return 1;
-    	
+    	if(args.length != 3)
+    	{
+    		System.out.println("Incorrect number of arguments.");
+    		return false;
+    	}
+
     	if(args[0].equals("boot"))
     	{
     		isBootPeer = true;
-    		int lim = Integer.parseInt(args[1]);
+    		
+    		int lim = Integer.parseInt(args[2]);
     		if(lim >= 1 && lim <= 1000)
     		{
     			peerLimit = lim;
     		}
     		else
     		{
-    			return 3;
+    			System.out.println("Error. Invalid peer limit. Should be 1-1000.");
+    			return false;
     		}
     	}
     	else if(args[0].equals("normal"))
     	{
     		isBootPeer = false;
-    		if(args[1].matches("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}:[0-9]{1,5}"))
+    		if(args[2].matches("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}:[0-9]{1,5}"))
     		{
-    			bootPeerIP = args[1];
+    			String[] bootPeerAddress = args[2].split(":");
+    			bootPeerIP = bootPeerAddress[0];
+    			bootPeerPort = Integer.parseInt(bootPeerAddress[1]);
     		}
     		else
-    			return 4;
+    		{
+    			System.out.println("Error. Invalid IP:PORT format.");
+    			return false;
+    		}
     	}
-    	else 
-    		return 2;
+    	else
+    	{
+    		System.out.println("Error. First argument should be 'boot' or 'normal'.");
+    		return false;
+    	}
     	
-		return 0;
+		if(args[1].matches("[0-9]{1,5}"))
+		{
+			port = Integer.parseInt(args[1]);
+		}
+		else
+		{
+			System.out.println("Error. Invalid port.");
+			return false;
+		}
+
+		System.out.println("Setup successuful.");
+		return true;
     }
     
-    public static String getPublicIP() {
-        URL whatismyip = new URL("http://checkip.amazonaws.com");
-        BufferedReader in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));
-        return in.readLine();
+    public static void connectToBootPeer() throws IOException {
+    	
+        socket = new DatagramSocket();
+    	
+    	String msg = ip + ":" + port;
+       	byte[] data = msg.getBytes();
+       	
+    	InetAddress bootPeerAddress = InetAddress.getByName(bootPeerIP);
+ 
+        DatagramPacket packet = new DatagramPacket(data, data.length, bootPeerAddress, bootPeerPort);
+        socket.send(packet);
+        
     }
+    
+    public static void receiveNewPeer() throws IOException {
+        
+    	socket = new DatagramSocket(port);
+    	
+    	byte[] buffer = new byte[256];
+    	DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+    	socket.receive(packet);
+     
+    	String msg = new String(packet.getData(), 0, packet.getLength());  	
+    	String[] addressParts = msg.split(":");
+    	Address address = new Address(addressParts[0], Integer.parseInt(addressParts[1]));
+    	
+    	forwardingTable.put(forwardingTable.size(), address);
+    	showForwardingTable();
 
+    }
+    
+    public static void showForwardingTable() {
+    	System.out.println("\nPeerID - Address");
+    	for(Map.Entry<Integer, Address> entry : forwardingTable.entrySet()) {
+            Integer key = entry.getKey();
+            Address add = entry.getValue();
+            System.out.println(key + " - " + add.getIp() + ":" + add.getPort());
+        }
+    }
+    
     public void send(Integer id, byte[] buf) throws IOException {
         Address address = forwardingTable.get(id);
         if(address == null){
@@ -133,30 +170,11 @@ public class Peer implements RMIInterface{
         socket.receive(packet);
         new PacketHandler(packet).run();
     }
-
-    //RMI Funtions
-    @Override
-    public String backup(String fileName, int replicationDegree, boolean enhancement) throws RemoteException {
-
+    
+    public static String getPublicIP() throws IOException {
+        URL whatismyip = new URL("http://checkip.amazonaws.com");
+        BufferedReader in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));
+        return in.readLine();
     }
 
-    @Override
-    public String restore(String fileName, boolean enhancement) throws RemoteException {
-
-    }
-
-    @Override
-    public String delete(String fileName, boolean enhancement) throws RemoteException {
-
-    }
-
-    @Override
-    public String reclaim(int space, boolean enhancement) throws RemoteException {
-
-    }
-
-    @Override
-    public String state() throws RemoteException {
-
-    }
 }

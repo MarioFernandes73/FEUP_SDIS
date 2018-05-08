24104aeaef716b57ebc9730cb9df11a62937b2c2
André Baptista <aca.baptista@gmail.com>
2018-05-08 01:01:19 +0100

    Update Peer.java

--- Second_Project/src/peer/Peer.java
+++ Second_Project/src/peer/Peer.java
@@ -1,50 +1,48 @@
 package peer;
 
-import messages.PacketHandler;
-import utils.Constants;
-
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.MalformedURLException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketException;
 import java.net.URL;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Peer {
 
-    private static Integer id;
+    private static String id;
     private static boolean isBootPeer;
     private static String ip;
     private static int port;
     private static String bootPeerIP;
     private static int bootPeerPort;
-    private static HashMap<Integer,Address> forwardingTable = new HashMap<>();
+    private static ArrayList<Address> forwardingTable = new ArrayList<Address>();
     private static int peerLimit;
     private static int networkSize;
-    private static DatagramSocket socket;
     
+    private static DatagramSocket sendSocket;
+    private static DatagramSocket receiveSocket;
+
     public static void main(String[] args) throws IOException {
 
     	if(!verifyArgs(args))
     		return;
     	
-        ip = getPublicIP();
+        ip = InetAddress.getLocalHost().getHostAddress();
+        //getPublicIP();
+        
+        id = ip + ":" + port;
 
         if(isBootPeer)
-        	receiveNewPeer();
+        {
+        	bootPeer();
+        }
         else
-        	connectToBootPeer();
-   
+        {
+        	normalPeer();
+        }
+       
     }
 
     public static boolean verifyArgs(String args[])
@@ -105,70 +103,60 @@ public class Peer {
 		return true;
     }
     
-    public static void connectToBootPeer() throws IOException {
+    public static void normalPeer() throws IOException
+    {
+    	sendSocket = new DatagramSocket();
     	
-        socket = new DatagramSocket();
-    	
-    	String msg = ip + ":" + port;
-       	byte[] data = msg.getBytes();
+       	byte[] data = id.getBytes();
        	
     	InetAddress bootPeerAddress = InetAddress.getByName(bootPeerIP);
- 
-        DatagramPacket packet = new DatagramPacket(data, data.length, bootPeerAddress, bootPeerPort);
-        socket.send(packet);
+        DatagramPacket sendPacket = new DatagramPacket(data, data.length, bootPeerAddress, bootPeerPort);
+        sendSocket.send(sendPacket);
         
-    }
-    
-    public static void receiveNewPeer() throws IOException {
-        
-    	socket = new DatagramSocket(port);
+    	receiveSocket = new DatagramSocket(port);
     	
     	byte[] buffer = new byte[256];
-    	DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-    	socket.receive(packet);
-     
-    	String msg = new String(packet.getData(), 0, packet.getLength());  	
-    	String[] addressParts = msg.split(":");
-    	Address address = new Address(addressParts[0], Integer.parseInt(addressParts[1]));
+    	DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
+    	receiveSocket.receive(receivePacket);
     	
-    	forwardingTable.put(forwardingTable.size(), address);
+    	String msg = new String(receivePacket.getData(), 0, receivePacket.getLength());  	
+    	System.out.println(msg);
+    }
+    
+    public static void bootPeer() throws IOException
+    {
+    	receiveSocket = new DatagramSocket(port);
+		
+    	byte[] buffer = new byte[256];
+    	DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
+    	receiveSocket.receive(receivePacket);
+    	 
+    	String msg = new String(receivePacket.getData(), 0, receivePacket.getLength());  	
+    	String[] addressParts = msg.split(":");
+    	String peerIP = addressParts[0];
+    	String peerPort = addressParts[1];
+    	Address peerAddress = new Address(peerIP, Integer.parseInt(peerPort));
+    	
+    	sendSocket = new DatagramSocket();
+    	
+    	String table = "Table\n";
+    	for(Address add : forwardingTable)
+    		table += add.getIp() + ":" + add.getPort() + "\n";
+    	 
+    	byte[] data = table.getBytes();
+    	 
+    	DatagramPacket sendPacket = new DatagramPacket(data, data.length, InetAddress.getByName(peerIP), Integer.parseInt(peerPort));
+    	sendSocket.send(sendPacket);
+    	    	
+    	forwardingTable.add(peerAddress);
     	showForwardingTable();
-
     }
-    
+
     public static void showForwardingTable() {
-    	System.out.println("\nPeerID - Address");
-    	for(Map.Entry<Integer, Address> entry : forwardingTable.entrySet()) {
-            Integer key = entry.getKey();
-            Address add = entry.getValue();
-            System.out.println(key + " - " + add.getIp() + ":" + add.getPort());
+    	System.out.println("\nForwarding Table");
+    	for(Address address : forwardingTable) {
+            System.out.println(address.getIp() + ":" + address.getPort());
         }
-    }
-    
-    public void send(Integer id, byte[] buf) throws IOException {
-        Address address = forwardingTable.get(id);
-        if(address == null){
-            Integer tempId = -1;
-            for(Map.Entry<Integer, Address> entry : forwardingTable.entrySet()) {
-                Integer key = entry.getKey();
-                if(key > tempId && key < id){
-                    tempId = key;
-                }
-            }
-            if(tempId == -1){
-                System.out.println("DEU -1 NO SEND PARA UM PEER DO PEER " + this.id);
-            }
-            address = forwardingTable.get(tempId);
-        }
-        DatagramPacket packet = new DatagramPacket(buf, buf.length, address.getInetAddress(), address.getPort());
-        socket.send(packet);
-    }
-
-    public void receive() throws IOException {
-        byte[] buf = new byte[Constants.MAX_PACKET_SIZE];
-        DatagramPacket packet = new DatagramPacket(buf, buf.length);
-        socket.receive(packet);
-        new PacketHandler(packet).run();
     }
     
     public static String getPublicIP() throws IOException {

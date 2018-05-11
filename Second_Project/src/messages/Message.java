88b91c409440fd20a8f02b38efa9980646b38402
Mário Fernandes <mgustavofernandes@gmail.com>
2018-05-12 00:08:37 +0100

    added message interpreter

--- Second_Project/src/messages/Message.java
+++ Second_Project/src/messages/Message.java
@@ -1,4 +1,18 @@
 package messages;
 
-public class Message {
+import peer.Peer;
+
+class Message {
+
+    private byte[] body;
+
+    String getHeader(){
+        return "";
+    }
+
+    void setBody(byte[] body){
+        this.body = body;
+    }
+
+    void receivedOperation(Peer peer) {}
 }
--- Second_Project/src/messages/MessageBuilder.java
+++ Second_Project/src/messages/MessageBuilder.java
@@ -1,9 +1,11 @@
 package messages;
 
+import java.util.ArrayList;
+
 public class MessageBuilder {
 
-    public Message build(String[] args){
-        if(args[0] == "PUTCHUNK"){
+    public Message build(ArrayList<String> args){
+        if(args.get(0).equals("PUTCHUNK")){
             return new MessagePutchunk();
         }
         return null;

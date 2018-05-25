b3535b50de6515f36edbfd2ebcda87c583397300
Nelson Almeida <up201505394@fe.up.pt>
2018-05-25 13:05:10 +0100

    Add files via upload

---
+++ Second_Project/src/protocols/initiators/RestoreInitiator.java
@@ -0,0 +1,72 @@
+package protocols.initiators;
+
+import client.Client;
+import filesmanager.BackedUpFileInfo;
+import filesmanager.Chunk;
+import peer.ChunkInfo;
+import peer.Peer;
+import protocols.protocols.ChunkBackupProtocol;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class RestoreInitiator implements Runnable {
+
+    private Client client;
+    private Peer peer;
+    private File file;
+    private int replicationDegree;
+
+    public RestoreInitiator(String peer, String fileId) {
+        this.peer = peer;
+        this.fileId = fileId;
+    }
+
+    @Override
+    public void run() {
+    	BackedUpFileInfo fileInfo = peer.getBackedUpFileInfo(fileId);
+
+        ArrayList<Chunk> chunks = this.peer.splitToChunks(file);
+        ArrayList<ChunkInfo> chunksInfo = new ArrayList<>();
+        ArrayList<Thread> protocolThreads = new ArrayList<>();
+
+        for (int i = 0; i < chunks.size(); i++) {
+            ChunkInfo chunkInfo = new ChunkInfo(encryptedFileId, i, replicationDegree, chunks.get(i).getData().length);
+            chunksInfo.add(chunkInfo);
+            Thread thread = new Thread(new ChunkBackupProtocol(this.peer, chunkInfo, chunks.get(i).getData()));
+            protocolThreads.add(thread);
+            this.peer.clearStoredMessagesOfFile(encryptedFileId);
+            thread.start();
+        }
+
+        for (Thread thread : protocolThreads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for(ChunkInfo chunkInfo: chunksInfo) {
+            if(chunkInfo.getOwnerIds().size() > 0) {
+
+                BackedUpFileInfo newBackedUpFile = new BackedUpFileInfo(encryptedFileId, file.getName(), file.lastModified(), true);
+                newBackedUpFile.getBackedUpChunks().addAll(chunksInfo);
+                this.peer.updateBackedUpFiles(newBackedUpFile);
+
+                if(chunkInfo.getOwnerIds().size() >= this.replicationDegree) {
+                    System.out.println("Successful backup of chunk "+ chunkInfo.getChunkNo());
+                } else {
+                    System.out.println("Successful backup of chunk " + chunkInfo.getChunkNo() +" but with a replication degree below the threshold");
+                }
+                System.out.println("Desired replication degree: " + this.replicationDegree);
+                System.out.println("Current replication degree: " + chunkInfo.getOwnerIds().size());
+            } else {
+                System.out.println("Unsuccessful backup of chunk " + chunkInfo.getChunkNo());
+            }
+        }
+    }
+
+
+}
+
